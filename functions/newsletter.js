3de0180fbb2d7cc67e5cdc0e648c0edd8de662bf
Blue Toby Studio <play.sonoklo@gmail.com>
2026-06-10 11:59:16 +0200

    Adding backend

---
+++ functions/newsletter.js
@@ -0,0 +1,8 @@
+export async function subscribeToNewsletter(context) {
+  try {
+    const { email } = await context.request.json().email;
+    return new Response(JSON.stringify({ message: `Sub with email: ${email}` }), { status: 200 });
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), { status: 500 });
+  }
+}
--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const closeButton = document.getElementById("ncb")
+const submitButton = document.getElementById("nsb")
+const emailInput = document.getElementById("nei")
+const nForm = document.getElementById("nf")
 if (closeButton) {
-  closeButton.addEventListener("click", onCloseNewsletter)
+  closeButton.addEventListener("click", onCloseNewsletter);
+}
+if (submitButton) {
+  submitButton.addEventListener("click", onSubmitNewsletter);
+}
+if (nForm) {
+  nForm.addEventListener('submit', function(event) { event.preventDefault(); } )
 }
 
 function onCloseNewsletter() {
-  const popup = document.getElementById("npp")
+  const popup = document.getElementById("npp");
   if (popup) {
-    popup.classList.add("hide")
+    popup.classList.add("hide");
   }
 }
 
-
+async function onSubmitNewsletter(event) {
+  const emailValue = emailInput.value;
+  const response = await fetch('/functions/subscribe', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email: emailValue })
+  });
+  console.log(`sent: ${emailValue}`)
+  event.preventDefault();
+}
